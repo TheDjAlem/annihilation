eb57770422dbb15ea0bea0fb74c1d4f3bb4f170b
TheDjAlem <78572724+TheDjAlem@users.noreply.github.com>
2024-01-27 22:39:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function updateCountdown() {
+    const targetDate = new Date('March 1 2024').getTime();
+    const currentDate = new Date().getTime();
+    const timeDifference = targetDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    if (timeDifference < 0) {
+        // This is here to prevent the countdown from going negative Alem :)
+        document.getElementById('days').innerText = '00';
+        document.getElementById('hours').innerText = '00';
+        document.getElementById('minutes').innerText = '00';
+        document.getElementById('seconds').innerText = '00';
+        return;
+    }
+
+    document.getElementById('days').innerText = formatTime(days);
+    document.getElementById('hours').innerText = formatTime(hours);
+    document.getElementById('minutes').innerText = formatTime(minutes);
+    document.getElementById('seconds').innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
