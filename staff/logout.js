e6064af912fcdf90dbf8a284135a091217907add
TheDjAlem <78572724+TheDjAlem@users.noreply.github.com>
2024-02-12 11:24:19 -0500

    Update logout.js

--- staff/logout.js
+++ staff/logout.js
@@ -2,7 +2,7 @@
 function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
-        window.location.href = "index.html"; // Redirect to login page
+        window.location.href = "/staff/index.html"; // Redirect to login page within the /staff/ folder
     }).catch((error) => {
         // An error happened.
         console.error(error);
